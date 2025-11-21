0125a52e9adf73b4a1cc0571cc7a2ab2fcefd8f0
Hana Atinaf <hanaatianf2@gmail.com>
2025-11-21 15:48:44 -0700

    Working on testing

--- src/test/java/classes/ProviderTest.java
+++ src/test/java/classes/ProviderTest.java
@@ -1,11 +1,27 @@
 package classes;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProviderTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getInfo() {
+        // arrange
+        Provider provider = new Provider("Yearup United ", "hana@yearupUnited.org");
 
+        // act
+        String info = provider.getInfo();
+
+        // assertions
+         // check provider name
+        assertEquals("Yearup United", provider.getName());
+        //  check provider info
+        assertEquals("hana@yearupUnited.org", provider.getInfo());
+
+        // check full getInfo() output
+        assertEquals(
+                "Provider : Yearup United | Contact :  hana@yearupUnited.org", info );
     }
 }
